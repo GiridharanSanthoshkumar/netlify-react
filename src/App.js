6df1eb2b0fd6afc4f9758ff06d28b146ee78353a
Giridharansanthoshkumar <giridharansanthosh0501@gmail.com>
2024-07-16 15:40:39 +0530

    set upready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import data from "./data.json";
+let image=data;
+function App()
+{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+
+    
+    return <div>
+        <h1>hello</h1>
+        {image.map((i)=>{return <h1>veryy goof</h1>})}
+
     </div>
-  );
 }
-
 export default App;
